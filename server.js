969b7f8d20b2e3bc0ac81de2191356eda8218e0b
Jess Hause Ugray <jess.ugray@gmail.com>
2021-11-17 17:32:33 -0500

    sequelize import typo fixed

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
-const Sequelize = require('./config/connection');
+const sequelize = require('./config/connection');
 
 const app = express();
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3002;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
